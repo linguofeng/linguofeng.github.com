7c7ecb1e5220f00637387a8de1a43f9ef128829d
linguofeng <linguofeng@msn.com>
2012-08-29 15:48:17 +0800

    update

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,18 +1,21 @@
-!function ($) {
+!function($) {
+	$(function() {
+		var $window = $(window)
 
-  $(function(){
+		// 左侧导航栏
+		$('.sidenav').affix({
+			offset : {
+				top : function() {
+					return $window.width() <= 980 ? 290 : 210
+				},
+				bottom : 270
+			}
+		})
 
-    var $window = $(window)
+		// 代码高亮
+		window.prettyPrint && prettyPrint()
 
-    // side bar
-    $('.sidenav').affix({
-      offset: {
-        top: function () { return $window.width() <= 980 ? 290 : 210 }
-      , bottom: 270
-      }
-    })
-    
-    // make code pretty
-    window.prettyPrint && prettyPrint()
-  })
+		// 工具提示
+		$("a[rel=tooltip]").tooltip()
+	})
 }(window.jQuery)
