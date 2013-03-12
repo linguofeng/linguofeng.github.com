12d1296fb58930c0c86901ac0027e0254bb173a6
linguofeng <linguofeng@msn.com>
2013-03-12 09:48:45 +0800

    new version

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,16 +2,6 @@
 	$(function() {
 		var $window = $(window)
 
-		// 左侧导航栏
-		$('.sidenav').affix({
-			offset : {
-				top : function() {
-					return $window.width() <= 980 ? 290 : 210
-				},
-				bottom : 270
-			}
-		})
-
 		// 代码高亮
 		window.prettyPrint && prettyPrint()
 
